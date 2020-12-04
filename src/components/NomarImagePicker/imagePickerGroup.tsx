2c3fd5585295ef81f766e093eab6e3ddfa210bf9
chenshuhang <437132124@qq.com>
2020-12-04 19:08:39 +0800

    recode: imagePicker

---
+++ src/components/NomarImagePicker/imagePickerGroup.tsx
@@ -0,0 +1,41 @@
+import React, { FC } from 'react';
+import { ImagePicker, Toast } from 'antd-mobile';
+import { ImageFile, ImagePickerGroupProps } from './interface';
+import { transformFile } from '../../utils';
+
+const ImagePickerGroup: FC<ImagePickerGroupProps> = props => {
+  const { onChange, initValue = [], limitSize, compressRatio, ...otherProps } = props;
+
+  const checkFileLimit = (file: ImageFile) => {
+    if (limitSize && file && file.size && file.size > limitSize) {
+      Toast.fail('图片过大', 1);
+      return false;
+    }
+    return true;
+  };
+
+  const imageChange = (files: ImageFile[], operationType: string, index: number | undefined) => {
+    if (files && files.length > initValue.length) {
+      const lastFile = files[files.length - 1];
+      const { file = {} } = lastFile;
+      if (compressRatio && lastFile.url.indexOf('base64,') !== -1) {
+        transformFile(lastFile.file, compressRatio).then((newFile: any) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(newFile);
+          reader.onload = function({ target }) {
+            if (!checkFileLimit(newFile)) return;
+            files[files.length - 1] = {
+              ...files[files.length - 1],
+              file: newFile,
+              url: target?.result || '',
+            };
+          };
+        });
+      } else if (!checkFileLimit(file)) return;
+    }
+    onChange(files, operationType, index);
+  };
+  return <ImagePicker {...otherProps} onChange={imageChange} files={initValue} />;
+};
+
+export default ImagePickerGroup;
--- src/components/NomarImagePicker/index.tsx
+++ src/components/NomarImagePicker/index.tsx
@@ -1,26 +1,12 @@
 import React, { FC, useState } from 'react';
-import { Rule } from 'rc-field-form/es/interface';
-import { ImagePicker, Toast } from 'antd-mobile';
-import { ImagePickerPropTypes } from 'antd-mobile/es/image-picker/PropsType';
+
+import ImagePickerGroup from './imagePickerGroup';
+import { ImageFile, INomarImagePickerProps } from './interface';
 import Field from '../Field';
-import { transformFile } from '../../utils';
 import '../../styles/index.less';
 
-export interface INomarImagePickerProps extends ImagePickerPropTypes {
-  coverStyle?: React.CSSProperties;
-  title?: string;
-  required?: boolean;
-  fieldProps: string;
-  rules?: Rule[];
-  hasStar?: boolean;
-  limitSize?: number;
-  subTitle?: string | React.ReactNode;
-  hidden?: boolean;
-  extra?: string | React.ReactNode;
-  compressRatio?: number;
-}
-
 const NomarImagePicker: FC<INomarImagePickerProps> = props => {
+  const [initValue, setInitValue] = useState([]);
   const {
     coverStyle,
     title,
@@ -32,20 +18,11 @@ const NomarImagePicker: FC<INomarImagePickerProps> = props => {
     subTitle,
     hidden = false,
     extra = '',
-    compressRatio,
     onChange,
     ...otherProps
   } = props;
-  const [fileList, setFileList] = useState([]);
 
-  const imageChange = (files: any, operationType: string, index: number | undefined) => {
-    if (files.length >= 0) {
-      const lastFile = files[files.length - 1];
-      const { file = {} } = lastFile;
-      if (limitSize && file && file.size && file.size > limitSize) {
-        return;
-      }
-    }
+  const imageChange = (files: ImageFile[], operationType: string, index: number | undefined) => {
     if (onChange) onChange(files, operationType, index);
   };
 
@@ -67,33 +44,16 @@ const NomarImagePicker: FC<INomarImagePickerProps> = props => {
             name={fieldProps}
             rules={rules || [{ required, message: `请选择${title}` }]}
             shouldUpdate={(prevValue: any, nextValue: any) => {
-              const files = nextValue[fieldProps] || [];
-              if (files && files.length > fileList.length) {
-                const lastFile = files[files.length - 1];
-                const { file = {} } = lastFile;
-                if (limitSize && file && file.size && file.size > limitSize) {
-                  Toast.fail('图片过大', 1);
-                  return false;
-                }
-                if (compressRatio && lastFile.url.indexOf('base64,') !== -1) {
-                  transformFile(lastFile.file, compressRatio).then((newFile: any) => {
-                    const reader = new FileReader();
-                    reader.readAsDataURL(newFile);
-                    reader.onload = function({ target }) {
-                      files[files.length - 1] = {
-                        ...files[files.length - 1],
-                        file: newFile,
-                        url: target?.result,
-                      };
-                    };
-                  });
-                }
-              }
-              setFileList((nextValue && nextValue[fieldProps as any]) || []);
+              setInitValue(nextValue && nextValue[fieldProps as any]);
               return prevValue !== nextValue;
             }}
           >
-            <ImagePicker {...otherProps} onChange={imageChange} files={fileList} />
+            <ImagePickerGroup
+              {...otherProps}
+              onChange={imageChange}
+              initValue={initValue}
+              limitSize={limitSize}
+            />
           </Field>
         </div>
       )}
