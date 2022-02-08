6f0baf3536abf2c865de579e74bd553718426faf
陈书航 <>
2022-02-08 11:31:56 +0800

    feat: recode textarea components

--- src/components/NomarTextArea/TextareaItem.tsx
+++ src/components/NomarTextArea/TextareaItem.tsx
@@ -1,48 +1,129 @@
-import React, { FC, CSSProperties, useState } from 'react';
+import React, { FC, useRef, useEffect, ReactNode } from 'react';
 import classNames from 'classnames';
 import { allPrefixCls } from '../../const';
 
+import { TextareaItemProps } from './interface';
 import './index.less';
-
-interface TextareaItemProps {
-  className?: string;
-  style?: CSSProperties;
-  placeholder?: string;
-  value?: string;
-  onChange: (res: string) => void;
-}
 
 const prefixCls = `${allPrefixCls}-textarea`;
 
-const TextareaItem: FC<TextareaItemProps> = (props) => {
+interface ITextareaItemProps extends TextareaItemProps {
+  title: ReactNode;
+  isVertical: boolean;
+}
+
+const TextareaItem: FC<ITextareaItemProps> = (props) => {
+  const nativeTextAreaRef = useRef<HTMLTextAreaElement>(null);
   const {
     className,
-    style,
+    coverStyle,
     placeholder = '请输入',
     value = '',
     onChange,
+    title,
+    id,
+    maxLength,
+    showCount,
+    onClick,
+    disabled = false,
+    editable = true,
+    autoSize = false,
+    positionType = 'horizontal',
+    labelNumber = 5,
+    isVertical,
+    ...otherProps
   } = props;
+
+  useEffect(() => {
+    if (!autoSize) return;
+    const textArea = nativeTextAreaRef.current;
+    if (!textArea) return;
+    textArea.style.height = 'auto';
+    let height = textArea.scrollHeight;
+
+    if (typeof autoSize === 'object') {
+      const computedStyle = window.getComputedStyle(textArea);
+      const lineHeight = parseFloat(computedStyle.lineHeight);
+      if (autoSize.minRows) {
+        height = Math.max(height, autoSize.minRows * lineHeight);
+      }
+      if (autoSize.maxRows) {
+        height = Math.min(height, autoSize.maxRows * lineHeight);
+      }
+    }
+
+    textArea.style.height = `${height}px`;
+  }, [value, autoSize]);
 
   const areaChange = (e: string) => {
     if (onChange) onChange(e);
   };
+
+  let count: ReactNode;
+  if (typeof showCount === 'function') {
+    count = showCount(value.length, maxLength);
+  } else if (showCount) {
+    count = (
+      <div className={`${prefixCls}-count`}>
+        {maxLength === undefined
+          ? value.length
+          : value.length + '/' + maxLength}
+      </div>
+    );
+  }
+
+  const showTextareaDom = () => {
+    return (
+      <div
+        className={classNames({
+          [`${prefixCls}-content`]: true,
+          [`${prefixCls}-padding`]: !isVertical,
+        })}
+      >
+        <textarea
+          ref={nativeTextAreaRef}
+          {...otherProps}
+          placeholder={placeholder}
+          className={classNames(
+            {
+              [prefixCls]: true,
+              [`${prefixCls}-disabled`]: disabled,
+            },
+            className,
+          )}
+          readOnly={!editable || disabled}
+          maxLength={maxLength}
+          style={coverStyle}
+          value={value}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
+            areaChange(e.target.value);
+          }}
+          onFocus={(e) => {
+            props.onFocus?.(e.target.value);
+          }}
+          onBlur={(e) => {
+            props.onBlur?.(e.target.value);
+          }}
+          id={id}
+          onClick={(e: React.MouseEvent<HTMLElement>) => {
+            if (!disabled && onClick) onClick(e);
+          }}
+        />
+        {count}
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <textarea
-        placeholder={placeholder}
-        className={classNames(
-          {
-            [prefixCls]: true,
-          },
-          className,
-        )}
-        style={style}
-        value={value}
-        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => {
-          areaChange(e.target.value);
-        }}
-      />
-    </div>
+    <>
+      {!isVertical && (
+        <div className={`${prefixCls}-wrapper`}>
+          {title}
+          {showTextareaDom()}
+        </div>
+      )}
+      {isVertical && showTextareaDom()}
+    </>
   );
 };
 
---
+++ src/components/NomarTextArea/interface.ts
@@ -0,0 +1,46 @@
+import { CSSProperties, ReactNode } from 'react';
+import { Rule } from 'rc-field-form/es/interface';
+
+export interface INomarTextAreaProps
+  extends Omit<TextareaItemProps, 'onChange'> {
+  required?: boolean;
+  fieldProps: string;
+  rules?: Rule[];
+  hasStar?: boolean;
+  extra?: React.ReactNode | string;
+  subTitle?: string | React.ReactNode;
+  hidden?: boolean;
+  defaultValue?: string;
+  errorValue?: any;
+  titleProps?: any;
+  formFlag?: boolean;
+  title?: string | ReactNode;
+  renderHeader?: string | React.ReactNode;
+  renderFooter?: string | React.ReactNode;
+  onChange?: (res: string) => void;
+}
+
+export interface TextareaItemProps {
+  className?: string;
+  coverStyle?: CSSProperties;
+  placeholder?: string;
+  value?: string;
+  onChange: (res: string) => void;
+  rows?: number;
+  id?: string;
+  onFocus?: (e: string) => void;
+  onBlur?: (e: string) => void;
+  maxLength?: number;
+  showCount?: boolean | ((length: number, maxLength?: number) => ReactNode);
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+  disabled?: boolean;
+  editable?: boolean;
+  positionType?: 'vertical' | 'horizontal';
+  labelNumber?: number;
+  autoSize?:
+    | boolean
+    | {
+        minRows?: number;
+        maxRows?: number;
+      };
+}
