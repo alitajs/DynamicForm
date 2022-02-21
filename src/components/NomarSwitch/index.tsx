64f113909bbc98db52119168d16baaa4a871b2f4
DIYC <xue.shidongyu110@iwhalecloud.com>
2022-02-21 16:07:52 +0800

    feat: complete the development of switch component compatible with PC effect requirements

--- src/components/NomarSwitch/index.tsx
+++ src/components/NomarSwitch/index.tsx
@@ -6,6 +6,13 @@ import { allPrefixCls } from '../../const/index';
 import Field from '../../baseComponents/Field';
 import Title from '../../baseComponents/Title';
 import './index.less';
+import PcLayout from '../../baseComponents/PcLayout';
+import HorizontalTitle from '../../baseComponents/HorizontalTitle';
+import { Switch as ASwitch } from 'antd';
+import classnames from 'classnames';
+
+const prefixCls = 'alitajs-dform-switch';
+const pcPrefixCls = 'alitajs-dform-pc-switch';
 
 export interface INomarSwitchProps extends SwitchProps {
   coverStyle?: React.CSSProperties;
@@ -22,6 +29,11 @@ export interface INomarSwitchProps extends SwitchProps {
   formFlag?: boolean;
   renderHeader?: string | React.ReactNode;
   renderFooter?: string | React.ReactNode;
+  isPc?: boolean;
+  labelNumber?: number;
+  positionType?: 'horizontal' | 'vertical';
+  subTitle?: string | React.ReactNode;
+  extra?: string | React.ReactNode;
 }
 
 const DformSwitch: FC<INomarSwitchProps> = (props) => {
@@ -38,29 +50,114 @@ const DformSwitch: FC<INomarSwitchProps> = (props) => {
     defaultValue = false,
     titleProps,
     formFlag = false,
+    isPc = false,
+    positionType = 'horizontal',
+    labelNumber = 5,
+    subTitle = '',
+    extra,
     ...otherProps
   } = props;
-  return (
-    <Title independentProps={props} formFlag={formFlag} {...titleProps}>
-      {!hidden && (
-        <div className={`${allPrefixCls}-switch`}>
-          <div className={`${allPrefixCls}-title`}>
-            {required && hasStar && (
-              <div className={`${allPrefixCls}-redStar`}>*</div>
-            )}
-            <div>{title}</div>
-          </div>
+
+  const isVertical = positionType === 'vertical';
+
+  const labelCls = classnames({
+    [`${allPrefixCls}-input-label-0`]: labelNumber === 0,
+    [`${allPrefixCls}-input-label-2`]: labelNumber === 2,
+    [`${allPrefixCls}-input-label-3`]: labelNumber === 3,
+    [`${allPrefixCls}-input-label-4`]: labelNumber === 4,
+    [`${allPrefixCls}-input-label-5`]: labelNumber === 5,
+    [`${allPrefixCls}-input-label-6`]: labelNumber === 6,
+    [`${allPrefixCls}-input-label-7`]: labelNumber === 7,
+  });
+
+  const renderPcContent = () => {
+    return (
+      <PcLayout
+        isVertical={isVertical}
+        left={
+          <HorizontalTitle
+            required={required}
+            hasStar={hasStar}
+            title={title}
+            labelNumber={labelNumber}
+            isVertical={isVertical}
+          />
+        }
+        right={
           <Field
             name={fieldProps}
             valuePropName="checked"
             rules={[{ required, message: `请选择${title}` }, ...(rules || [])]}
+            shouldUpdate={(prevValue: any, nextValue: any) => {
+              return prevValue !== nextValue;
+            }}
+            initialValue={defaultValue}
+            formFlag={formFlag}
+          >
+            <ASwitch checked={defaultValue} {...otherProps}></ASwitch>
+          </Field>
+        }
+      />
+    );
+  };
+
+  const renderDefault = () => {
+    return (
+      <div
+        className={classnames({
+          [prefixCls]: true,
+          [`${prefixCls}-vertical`]: isVertical,
+        })}
+      >
+        {!isVertical && (
+          <div
+            className={classnames(labelCls, {
+              [`${allPrefixCls}-title`]: true,
+              [`${allPrefixCls}-vertical-title`]: isVertical,
+            })}
+          >
+            <HorizontalTitle
+              required={required}
+              hasStar={hasStar}
+              title={title}
+              labelNumber={labelNumber}
+              isVertical={isVertical}
+            />
+          </div>
+        )}
+        <div style={coverStyle}>
+          <Field
+            name={fieldProps}
+            valuePropName="checked"
+            rules={[{ required, message: `请选择${title}` }, ...(rules || [])]}
+            shouldUpdate={(prevValue: any, nextValue: any) => {
+              return prevValue !== nextValue;
+            }}
             initialValue={defaultValue}
             formFlag={formFlag}
           >
             <Switch checked={defaultValue} {...otherProps} />
           </Field>
         </div>
-      )}
+      </div>
+    );
+  };
+
+  return (
+    <Title
+      positionType={positionType}
+      hidden={hidden}
+      required={required}
+      hasStar={hasStar}
+      title={title}
+      subTitle={subTitle}
+      extra={extra}
+      isPc={props.isPc}
+      {...titleProps}
+    >
+      <div className={`${allPrefixCls}-field`}>
+        {!!isPc ? renderPcContent() : renderDefault()}
+      </div>
     </Title>
   );
 };
