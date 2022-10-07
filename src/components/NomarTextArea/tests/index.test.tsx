ef2bef079743f5b2625c851c49e6e3031b3e93db
OBlivion <915019672@qq.com>
2022-10-07 21:24:47 +0800

    test: add textArea single use test case

--- src/components/NomarTextArea/tests/index.test.tsx
+++ src/components/NomarTextArea/tests/index.test.tsx
@@ -3,6 +3,7 @@ import { render, testA11y, fireEvent, waitFor, sleep } from '@alita/test';
 import Form from 'rc-field-form';
 import DformTextArea from '../index';
 import BasicText from './demos/basic';
+import SingleText from './demos/single';
 
 const mtProps = {
   type: 'area',
@@ -71,9 +72,34 @@ test('renders Basic', async () => {
   const ID: any =
     getAllByText('身份证')[1].parentNode?.parentNode?.parentNode?.lastChild
       ?.lastChild;
+
+  expect(ID.getAttribute('placeholder') === '存在 extra 自动换行').toBe(true);
+
   fireEvent.change(ID, { target: { value: '身份证' } });
   fireEvent.click(getByText('Submit'));
   await waitFor(() => {
     expect(onFinish).toBeCalled();
   });
+});
+
+test('single basic', async () => {
+  const onChange = jest.fn();
+  const onBlur = jest.fn();
+  const { getAllByText } = render(
+    <SingleText onChange={onChange} onBlur={onBlur} />,
+  );
+  const area: any =
+    getAllByText('文本框')[0].parentNode?.parentNode?.parentNode?.lastChild
+      ?.lastChild;
+  expect(area.getAttribute('placeholder') === '请输入').toBe(true);
+  expect(area).toHaveValue('123');
+
+  area.focus();
+  area.blur();
+  expect(onBlur).toBeCalled();
+
+  fireEvent.change(area, { target: { value: 'test' } });
+  expect(onChange).toBeCalled();
+  area.blur();
+  expect(area).toHaveValue('test');
 });
